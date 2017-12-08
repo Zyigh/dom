a042bd968ca573d42e5e43bca864b92d1f9e6fc7
Hugo Medina <hmedina.paris75@gmail.com>
2017-12-08 16:45:03 +0100

    event YAAAAAAAAAY

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # dom
 
 Simple class over DOM and JS Events
+
+## SAUCES
+
+[!SAUUUUUUUUCES](assets/1490816528449.gif)
+
+* [Events](https://www.w3schools.com/js/js_events.asp)
+* [AddEventListener](https://developer.mozilla.org/fr/docs/Web/API/EventTarget/addEventListener)
--- real_dev_stuff/very_complicated_to_understand/my_awesome_app.js
+++ real_dev_stuff/very_complicated_to_understand/my_awesome_app.js
@@ -1,18 +1,84 @@
 var lis = document.querySelectorAll('main li');
-var nav_lis = document.querySelectorAll('nav li');
-var temp_array_for_reversing_word = [];
+// var nav_lis = document.querySelectorAll('nav li');
+// var temp_array_for_reversing_word = [];
+//
+// for (var i = 0, only_textual_lis = lis.length -1; i < only_textual_lis; i++) {
+//     lis[i].classList.add('barre');
+// }
+//
+// var i = 0;
+// for (var len = nav_lis.length; i < len; i++) {
+//     temp_array_for_reversing_word.push(nav_lis[i].innerHTML);
+// }
+// for (--i, current_word = 0; i >= 0; --i, current_word++) {
+//     nav_lis[i].innerHTML = temp_array_for_reversing_word[current_word];
+// }
+//
+// document.querySelector('header').setAttribute('style', 'background: lightblue');
+// document.querySelector('main').setAttribute('style', 'background: black; color: white');
 
-for (var i = 0, only_textual_lis = lis.length -1; i < only_textual_lis; i++) {
-    lis[i].classList.add('barre');
+
+// Events
+
+var header = document.querySelector('header');
+
+    // On self by using global selector
+
+//header.addEventListener('click', function(){
+//    header.classList.add('darkred');
+//});
+
+    // Toggle
+
+// header.addEventListener('click', function(){
+//     header.classList.toggle('darkred');
+// });
+
+    // On other using a global selector
+
+var main = document.querySelector('main');
+// header.addEventListener('click', function(){
+//     main.classList.toggle('darkred');
+// });
+
+    //Intro this
+
+// header.classList.add('darkred');
+// header.addEventListener('click', function(){
+//     this.classList.toggle('darkred');
+//     main.classList.toggle('darkred');
+// });
+
+    // handle list
+
+        //VERSION DE GROS PORC
+
+//for (let i = 0, len = lis.length; i < len - 1; i++) {
+//    lis[i].addEventListener('click', function(){
+//        lis[i].classList.toggle('barre');
+//    });
+//}
+
+        //UN PEU MIEUX VERSION
+
+//for (var i = 0, len = lis.length; i < len - 1; i++) {
+//    lis[i].addEventListener('click', function(){
+//        this.classList.toggle('barre');
+//    });
+//}
+
+        //Version propre
+
+//function barre_le_bordel() {
+//    this.classList.toggle('barre');
+//}
+
+for (var i = 0, len = lis.length; i < len - 1; i++) {
+    lis[i].addEventListener('click', barre_le_bordel);
 }
 
-var i = 0;
-for (var len = nav_lis.length; i < len; i++) {
-    temp_array_for_reversing_word.push(nav_lis[i].innerHTML);
-}
-for (--i, current_word = 0; i >= 0; --i, current_word++) {
-    nav_lis[i].innerHTML = temp_array_for_reversing_word[current_word];
-}
-
-document.querySelector('header').setAttribute('style', 'background: lightblue');
-document.querySelector('main').setAttribute('style', 'background: black; color: white');
+        // Remove
+//function barre_le_bordel() {
+//    this.classList.toggle('barre');
+//    this.removeEventListener('click', barre_le_bordel)
+//}
